7dc4729a05f52395521f91fafa06e44c0398d50a
Misiak283 <92733880+Misiak283@users.noreply.github.com>
2021-10-22 09:42:43 -0700

    Initialize v4-testing

---
+++ src/functions/is-labeled.ts
@@ -0,0 +1,26 @@
+import deburr from 'lodash.deburr';
+import {Issue} from '../classes/issue';
+import {ILabel} from '../interfaces/label';
+import {CleanLabel} from '../types/clean-label';
+
+/**
+ * @description
+ * Check if the given label is listed as a label of the given issue
+ *
+ * @param {Readonly<Issue>} issue A GitHub issue containing some labels
+ * @param {Readonly<string>} label The label to check the presence with
+ *
+ * @return {boolean} Return true when the given label is also in the given issue labels
+ */
+export function isLabeled(
+  issue: Readonly<Issue>,
+  label: Readonly<string>
+): boolean {
+  return !!issue.labels.find((issueLabel: Readonly<ILabel>): boolean => {
+    return cleanLabel(label) === cleanLabel(issueLabel.name);
+  });
+}
+
+function cleanLabel(label: Readonly<string>): CleanLabel {
+  return deburr(label.toLowerCase());
+}
